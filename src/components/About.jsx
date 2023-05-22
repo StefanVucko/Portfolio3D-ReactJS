fe0b9cdbc48e5563a38aa9cda6ef1a9d2495072c
Stefan Vuckovic <stefanvuckovic53@gmail.com>
2023-05-22 14:54:17 +0200

    Ajout du Projet

---
+++ src/components/About.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <h2 className={styles.sectionHeadText}>Qui suis je ?</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        Je suis un jeune développeur web en formation avec OpenClassroom qui recherche une entreprise pour l'accueillir.
+        Ayant déja effectuer une formation de développeur web et web mobile, je possède déja quelques bases en programmation.
+        Depuis cette formation que j'ai terminé avec succés, j'ai continué d'apprendre en autodidacte à travers plusieurs tutos en ligne,
+        mais aujourd'hui je souhaiterai passer à l'étape suivant et me lancé dans un vrai projet professionnel graçe à une formation en alternance.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/components/Works.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Works = () => {
+  return (
+    <div>Projets</div>
+  )
+}
+
+export default Works
